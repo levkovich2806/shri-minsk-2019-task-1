ddecc80be8b775a27b9825278ca35a6cae801b46
Sergey Levkovich <sergey.levkovich@rdigital.by>
2019-03-28 12:58:34 +0300

    вернул вывод заметок на экран через специальный класс NotesColl

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,26 @@
 import data from './shri';
-import Notes from '../ts/Notes.ts';
+//import Notes from "../ts/Notes.ts";
 import NotesColl from '../ts/NotesColl.ts';
 import createRandomNote from './helper';
 
-document.addEventListener('DOMContentLoaded', function () {
-
+document.addEventListener('DOMContentLoaded', function() {
   initPage();
 
-  const notes = Notes.factory(data.notes);
+  //const notes = Notes.factory(data.notes);
 
-  for (let i of notes) {
-    console.log(i);
-  }
+  // for (let i of notes) {
+  //   console.log(i);
+  // }
 
-  const btnAdd = document.getElementById("btnAdd");
-  btnAdd.addEventListener("click", (e) => {
+  const notesCollecion = NotesColl.factory(data);
+  notesCollecion.generateNotesHtml();
+
+  const btnAdd = document.getElementById('btnAdd');
+  btnAdd.addEventListener('click', e => {
     e.preventDefault();
     //Показываем модалку с формой для добавления новой заметки, пока сдесь будем вызывать статическое добавление рандомной заметки
     const newNote = createRandomNote();
-    data.notes = data.notes.concat(newNote);
-    notes.addNote(newNote);
+    notesCollecion.addNote(newNote);
   });
 });
 
@@ -30,19 +31,19 @@ function createTitleBlock({ color }) {
 }
 
 function initPage() {
-  const mainNav = document.getElementsByClassName("js-menu");
-  const navBarToggle = document.getElementById("js-navbar-toggle");
+  const mainNav = document.getElementsByClassName('js-menu');
+  const navBarToggle = document.getElementById('js-navbar-toggle');
 
-  navBarToggle.addEventListener("click", function () {
+  navBarToggle.addEventListener('click', function() {
     for (let i = 0; i < mainNav.length; ++i) {
       const item = mainNav[i];
-      item.classList.toggle("active");
+      item.classList.toggle('active');
     }
   });
 
-  const notesBlocks = document.getElementsByClassName("notes__title_blocks");
+  const notesBlocks = document.getElementsByClassName('notes__title_blocks');
 
-  data.colors.forEach((color) => {
+  data.colors.forEach(color => {
     notesBlocks[0].innerHTML += createTitleBlock(color);
   });
 }
