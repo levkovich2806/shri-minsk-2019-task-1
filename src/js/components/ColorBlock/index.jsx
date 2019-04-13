e9db3ac1b28b534b0ee1c2b969aa6251ae8f4733
Sergey Levkovich <sergey.levkovich@rdigital.by>
2019-04-13 17:56:20 +0300

    Сделал цветные блоки чекбоксами и прикрутил стили к поведению

--- src/js/components/ColorBlock/index.jsx
+++ src/js/components/ColorBlock/index.jsx
@@ -3,8 +3,13 @@ import styles from "./index.module.scss";
 
 class ColorBlock extends PureComponent {
   render() {
-    const { color = "orange" } = this.props;
-    return <div className={styles.block} style={{ backgroundColor: color }} />;
+    const { color = "orange", checked = true } = this.props;
+    return (
+      <label className={styles.container}>
+        <input type="checkbox" defaultChecked={checked} />
+        <span className={styles.checkmark} style={{ backgroundColor: color }} />
+      </label>
+    )
   }
 }
 
