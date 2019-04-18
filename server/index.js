04acc69b1ecd9692aac09c6cab8776780d57238a
Sergey Levkovich <sergey.levkovich@rdigital.by>
2019-04-18 20:12:22 +0300

    -

--- server/index.js
+++ server/index.js
@@ -15,12 +15,13 @@ const cards = require("./routes/cards.js");
 app.use("/api/cards", cards);
 
 // Все маршруты, кроме тех, что описаны выше (в нашем случае только /api/cards/*)  обрабатываем ниже
-app.use("*", (req, res) => {
-  return res.status(404).send("<h1>Page not found</h1>");
-});
 
 app.use(express.static("build"));
 app.get("*", (req, res) => res.sendFile(path.resolve("build", "index.html")));
+
+app.use("*", (req, res) => {
+  return res.status(404).send("<h1>Page not found</h1>");
+});
 
 app.listen(8000);
 
